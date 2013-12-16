b755b9cb47a1b021e8b1c125dcbc1365e908ef50
Dediggefedde <dediggefedde@gmx.net>
2013-12-16 18:00:47 +0100

    Creation
    Now it's online...

---
+++ source/Win8_TileColor/Form1.Designer.vb
@@ -0,0 +1,239 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.id = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 267)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(260, 27)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Refresh list"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AllowUserToResizeRows = False
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.White
+        Me.DataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.DataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.ColumnHeadersVisible = False
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column2, Me.id})
+        Me.DataGridView1.GridColor = System.Drawing.Color.White
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 29)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersVisible = False
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(260, 232)
+        Me.DataGridView1.TabIndex = 4
+        '
+        'Column2
+        '
+        Me.Column2.Frozen = True
+        Me.Column2.HeaderText = "Column2"
+        Me.Column2.Name = "Column2"
+        Me.Column2.ReadOnly = True
+        Me.Column2.Width = 260
+        '
+        'id
+        '
+        Me.id.HeaderText = "as"
+        Me.id.Name = "id"
+        Me.id.ReadOnly = True
+        Me.id.Visible = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.PictureBox1.Location = New System.Drawing.Point(289, 29)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(148, 127)
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(281, 162)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(125, 21)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "Display Name?"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(281, 216)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(169, 45)
+        Me.Button2.TabIndex = 7
+        Me.Button2.Text = "Save settings for selection"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(345, 81)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 17)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Text"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(281, 189)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(93, 21)
+        Me.RadioButton1.TabIndex = 9
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "bright font"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(373, 189)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(85, 21)
+        Me.RadioButton2.TabIndex = 9
+        Me.RadioButton2.Text = "dark font"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(281, 267)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(169, 27)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Set selection to default"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 7)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(200, 22)
+        Me.TextBox1.TabIndex = 11
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(211, 7)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(61, 23)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "search"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(36, 133)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(387, 23)
+        Me.ProgressBar1.TabIndex = 13
+        Me.ProgressBar1.Visible = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(286, 7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(142, 17)
+        Me.Label1.TabIndex = 14
+        Me.Label1.Text = "Click to set Backcolor"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(462, 303)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button4)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Win8_TileColor"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Column2 As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents id As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ source/Win8_TileColor/Form1.vb
@@ -0,0 +1,198 @@
+﻿Imports System.Security.Permissions
+Imports System.Security
+Imports Microsoft.Win32
+
+Public Class Form1
+    Private Structure linkup
+        Public path, name, rpath As String
+        Public id As Int16
+        Public co As Color
+        Public Sub New(ByVal p, ByVal n, ByVal r, ByVal i)
+            path = p : name = n : id = i : rpath = r
+        End Sub
+    End Structure
+    Private Function ReadLnk(ByVal filename As String) As String
+        Dim shortCut As IWshRuntimeLibrary.IWshShortcut
+        shortCut = CType((New IWshRuntimeLibrary.WshShell).CreateShortcut(filename), IWshRuntimeLibrary.IWshShortcut)
+        If (My.Computer.FileSystem.FileExists(shortCut.TargetPath)) Then
+            Return shortCut.TargetPath
+        Else
+            If (shortCut.TargetPath.Contains("Program Files (x86)")) Then
+                Return shortCut.TargetPath.Replace("Program Files (x86)", "Program Files")
+            ElseIf (shortCut.TargetPath.Contains("Program Files")) Then
+                Return shortCut.TargetPath.Replace("Program Files", "Program Files (x86)")
+            End If
+        End If
+    End Function
+    Dim keylist As New List(Of linkup)
+    Function parentfold(ByVal mpath As String, ByVal level As Int16) As String
+        Dim pathsec As String() = mpath.Split(IO.Path.DirectorySeparatorChar)
+        parentfold = ""
+        For i As Int16 = 0 To pathsec.Length - 1 - level
+            parentfold &= pathsec(i) & IO.Path.DirectorySeparatorChar
+        Next
+        Return parentfold
+    End Function
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        keylist.Clear()
+        DataGridView1.Rows.Clear()
+        Dim li(1) As IReadOnlyCollection(Of String)
+        li(0) = My.Computer.FileSystem.GetFiles(parentfold(Environment.GetFolderPath(Environment.SpecialFolder.CommonStartup), 2), FileIO.SearchOption.SearchAllSubDirectories, "*.lnk")
+        li(1) = My.Computer.FileSystem.GetFiles(My.Computer.FileSystem.SpecialDirectories.Programs, FileIO.SearchOption.SearchAllSubDirectories, "*.lnk")
+        Dim rex As System.Text.RegularExpressions.Regex = New System.Text.RegularExpressions.Regex("([^\\/]*?)\.lnk")
+        Dim rlink, rname As String
+        Dim usprung As Int16 = -1
+        For y As Int16 = 0 To 1
+            For x As Int16 = 0 To li(y).Count - 1
+                usprung += 1
+                rlink = ReadLnk(li(y)(x))
+                rname = rex.Match(li(y)(x)).Groups(1).Value
+                If (rlink = "" Or rname.Contains("Uninstall")) Then usprung -= 1 : Continue For
+                DataGridView1.Rows.Add(rname, usprung)
+                keylist.Add(New linkup(li(y)(x), rname, rlink, usprung))
+            Next
+        Next
+
+        Dim MeinKey, ukey As RegistryKey
+        MeinKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\GameUX\\Games")
+
+        If (MeinKey Is Nothing) Then MeinKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry64).OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\GameUX\\Games")
+        If (MeinKey Is Nothing) Then Exit Sub
+        Dim unams() As String = MeinKey.GetSubKeyNames
+        For Each s As String In unams
+            usprung += 1
+            ukey = MeinKey.OpenSubKey(s)
+            rname = ukey.GetValue("Title", "")
+            DataGridView1.Rows.Add(rname, usprung)
+            keylist.Add(New linkup(s, ukey.GetValue("Title", ""), ukey.GetValue("ConfigGDFBinaryPath", ""), usprung))
+            ukey.Close()
+        Next
+        MeinKey.Close()
+
+        DataGridView1.Sort(DataGridView1.Columns(0), System.ComponentModel.ListSortDirection.Ascending)
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        If (e.RowIndex < 0) Then Return
+        Label2.Text = keylist(DataGridView1.Item(1, e.RowIndex).Value).rpath
+
+
+        Dim nname As String = keylist(DataGridView1.Item(1, e.RowIndex).Value).rpath
+        If nname.LastIndexOf(".") <= 0 Then
+            nname &= ".VisualElementsManifest.xml"
+        End If
+        nname = nname.Substring(0, nname.LastIndexOf(".")) & ".VisualElementsManifest.xml"
+        If Not My.Computer.FileSystem.FileExists(nname) Then Return
+        Dim aktconf = My.Computer.FileSystem.ReadAllText(nname)
+        Dim aktcol As String = aktconf.Substring(aktconf.IndexOf("BackgroundColor=") + "BackgroundColor=".Length + 1)
+        aktcol = aktcol.Substring(0, aktcol.IndexOf(Chr(34)))
+        Dim aktshow As String = aktconf.Substring(aktconf.IndexOf("ShowNameOnSquare150x150Logo=") + "ShowNameOnSquare150x150Logo=".Length + 1)
+        aktshow = aktshow.Substring(0, aktshow.IndexOf(Chr(34)))
+        Dim aktfont As String = aktconf.Substring(aktconf.IndexOf("ForegroundText=") + "ForegroundText=".Length + 1)
+        aktfont = aktfont.Substring(0, aktfont.IndexOf(Chr(34)))
+        PictureBox1.BackColor = ColorTranslator.FromHtml(aktcol)
+        If aktshow = "on" Then CheckBox1.Checked = True Else CheckBox1.Checked = False
+        If aktfont = "light" Then RadioButton1.Checked = True : RadioButton2.Checked = False Else RadioButton1.Checked = False : RadioButton2.Checked = True
+
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        ProgressBar1.Visible = True
+        ProgressBar1.Maximum = DataGridView1.SelectedRows.Count + 3
+        ProgressBar1.Value = 0
+        Dim nname As String = ""
+        Dim highlight As String = "light"
+        Dim display As String = "off"
+        If (RadioButton2.Checked) Then highlight = "dark"
+        If (CheckBox1.Checked) Then display = "on"
+        Dim lognum As Integer = 0
+        For Each d As DataGridViewRow In DataGridView1.SelectedRows
+            ProgressBar1.Value += 1
+            Application.DoEvents()
+            nname = keylist(d.Cells(1).Value).rpath
+            If nname.LastIndexOf(".") <= 0 Then
+                nname &= ".VisualElementsManifest.xml"
+            End If
+            nname = nname.Substring(0, nname.LastIndexOf(".")) & ".VisualElementsManifest.xml"
+            'Dim wp As New FileIOPermission(FileIOPermissionAccess.Write, nname)
+            ' If SecurityManager.IsGranted(wp) Then
+            Try
+                My.Computer.FileSystem.WriteAllText(nname, My.Resources.template.Replace("##3", highlight).Replace("##2", display).Replace("##1", "#" & PictureBox1.BackColor.ToArgb().ToString("X").Substring(2)), False)
+                If (keylist(d.Cells(1).Value).path(0) <> "{") Then My.Computer.FileSystem.GetFileInfo(keylist(d.Cells(1).Value).path).LastWriteTime = Now
+            Catch ex As Exception
+                lognum += 1
+                My.Computer.FileSystem.WriteAllText(My.Application.Info.DirectoryPath & "\AccessException.log", Now.ToString & vbCrLf & "Permission denied:" & vbCrLf & nname & vbCrLf, True)
+            End Try
+            ' End If
+        Next
+        If (lognum > 0) Then MsgBox("Permission denied to create files in " & lognum & " Cases." & vbCrLf & "See AccessException.log for detailed information!")
+        ProgressBar1.Visible = False
+    End Sub
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        Dim co As New ColorDialog
+        If co.ShowDialog() = DialogResult.Cancel Then Exit Sub
+        PictureBox1.BackColor = co.Color
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Label2.Parent = PictureBox1
+        Label2.Width = PictureBox1.Width
+        '   Label2.Top = (PictureBox1.Height - Label2.Height) / 2
+        '  Label2.Left= (PictureBox1.Width - Label2.Width) / 2
+        Label2.Dock = DockStyle.Fill
+        Label2.TextAlign = ContentAlignment.MiddleCenter
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+        If (RadioButton1.Checked) Then Label2.ForeColor = Color.White Else Label2.ForeColor = Color.Black
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+        If (RadioButton1.Checked) Then Label2.ForeColor = Color.White Else Label2.ForeColor = Color.Black
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+        Dim co As New ColorDialog
+        If co.ShowDialog() = DialogResult.Cancel Then Exit Sub
+        PictureBox1.BackColor = co.Color
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If (DataGridView1.RowCount = 0) Then Exit Sub
+        Dim r As New System.Text.RegularExpressions.Regex(TextBox1.Text, System.Text.RegularExpressions.RegexOptions.IgnoreCase)
+        For x As Integer = DataGridView1.CurrentCell.RowIndex + 1 To DataGridView1.RowCount - 1
+            If r.IsMatch(DataGridView1.Item(0, x).Value.ToString) Then
+                DataGridView1.FirstDisplayedScrollingRowIndex = x
+                DataGridView1.CurrentCell = DataGridView1.Item(0, x)
+                Exit Sub
+            End If
+        Next
+
+        DataGridView1.FirstDisplayedScrollingRowIndex = 0
+        DataGridView1.CurrentCell = DataGridView1.Item(0, 0)
+        MsgBox("Expression not found." & vbCrLf & " Please escape characters like .+?* with an \: \. \+ \? \*")
+    End Sub
+
+    Private Sub TextBox1_KeyUp(sender As Object, e As KeyEventArgs) Handles TextBox1.KeyUp
+        If e.KeyCode = Keys.Enter Then
+            Button4_Click(sender, e)
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        PictureBox1_Click(PictureBox1, EventArgs.Empty)
+    End Sub
+End Class
